77989c32804e444acb581aa4f3a7940f4e74ddac
roberto montalti <roberto.montalti00@gmail.com>
2024-01-19 12:28:05 +0100

    wip: working on a cross-platform port
    TODO: figure out why the process crashes on dotnet test XD

--- src/MongoDB.Embedded/libz.cs
+++ src/MongoDB.Embedded/libz.cs
@@ -1,33 +1,51 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
+using Microsoft.Win32;
 
 namespace MongoDB.Embedded
 {
-   internal class libz
+    internal class libz
     {
         public static object ReadSubKeyValue(string subKey, string keyName)
         {
             using (var rkSubKey = Registry.LocalMachine.OpenSubKey(subKey))
             {
                 if (rkSubKey == null)
-                    throw new Exception(string.Format(@"Error while reading registry key: {0}\{1} does not exist!", subKey, keyName));
-                
+                    throw new Exception(
+                        string.Format(
+                            @"Error while reading registry key: {0}\{1} does not exist!",
+                            subKey,
+                            keyName
+                        )
+                    );
+
                 try
                 {
                     var result = rkSubKey.GetValue(keyName);
                     rkSubKey.Close();
                     return result;
                 }
-                catch (Exception ex)   //This exception is thrown
-                { 
-                    throw new Exception(string.Format("Error while reading registry key: {0} param: {1}. ErrorMessage: {2}", subKey, keyName, ex.Message));
+                catch (Exception ex) //This exception is thrown
+                {
+                    throw new Exception(
+                        string.Format(
+                            "Error while reading registry key: {0} param: {1}. ErrorMessage: {2}",
+                            subKey,
+                            keyName,
+                            ex.Message
+                        )
+                    );
                 }
             }
         }
 
         internal static int GetWindowsBuildNumber()
         {
-            return Convert.ToInt32(ReadSubKeyValue(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\", "CurrentBuildNumber"));
+            return Convert.ToInt32(
+                ReadSubKeyValue(
+                    @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\",
+                    "CurrentBuildNumber"
+                )
+            );
         }
     }
 }
